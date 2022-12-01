7755bfb1c666bb732e7f06b12bfab78f39c223fc
Lucas Vinícius Carmassi <carmassilucas@gmail.com>
2022-11-30 23:59:14 -0300

    creating useCase specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,25 +1,12 @@
 import { Router } from 'express';
 
-import { SpecificationRepository } from '../modules/cars/repositories/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
+import { listSpecificationsController } from '../modules/cars/useCases/listSpecifications';
 
 const specificationRoutes = Router();
-const specificationsRepository = new SpecificationRepository();
 
-specificationRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
+specificationRoutes.post('/', (request, response) => { createSpecificationController.handle(request, response); });
 
-  const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
-});
-
-specificationRoutes.get('/', (request, response) => {
-  const specifications = specificationsRepository.list();
-
-  return response.json(specifications);
-});
+specificationRoutes.get('/', (request, response) => { listSpecificationsController.handle(request, response); });
 
 export { specificationRoutes };
